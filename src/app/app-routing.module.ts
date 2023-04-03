46c2d5c3bcda877b001fdb196b8691f3ae493ae7
humaidokasha@gmail.com <humaidokasha@gmail.com>
2023-04-04 00:09:53 +0500

    commit login /signup work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,36 @@ import { AuthComponent } from './layout/auth/auth.component';
 import { MainComponent } from './layout/main/main.component';
 import { PrivateModule } from './private/private.module';
 import { CanActivateAuthLayoutGuard } from './services/guards/can-activate-auth-layout.guard';
+import { SignUpComponent } from './layout/sign-up/sign-up.component';
+import { EvidenceComponent } from './layout/evidence/evidence.component';
+import { DisclaimerComponent } from './layout/disclaimer/disclaimer.component';
+import { ForgotPasswordComponent } from './layout/forgot-password/forgot-password.component';
+import { StartComponent } from './layout/start/start.component';
 
 const routes: Routes = [
   {
     path:'auth',
     component:AuthComponent
+  },
+  {
+    path:'signup',
+    component:SignUpComponent
+  },
+  {
+    path:'disclaimer',
+    component:DisclaimerComponent
+  },
+  {
+    path:'evidence',
+    component:EvidenceComponent
+  },
+  {
+    path:'forgotpassword',
+    component:ForgotPasswordComponent
+  },
+  {
+    path:'signup-start',
+    component:StartComponent
   },
   {
     path:'',
